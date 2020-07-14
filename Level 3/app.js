c48515fa193bbbf5e9cc0e19877932b273e15e6b
Sunit Patwardhan <65279280+Sunit130@users.noreply.github.com>
2020-07-14 15:23:16 +0530

    Add files via upload

---
+++ Level 3/app.js
@@ -0,0 +1,78 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const md5 = require("md5");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true, useUnifiedTopology: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+const User = new mongoose.model("User", userSchema);
+
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+
+
+app.route("/login")
+
+.get(function(req,res){
+    res.render("login");
+})
+
+.post(function(req, res){
+    const femail = req.body.username;
+    const fpassword = md5(req.body.password);
+    User.findOne({ email:femail }, function(err, foundUser){
+        if(!err) {
+            if(foundUser.password === fpassword) {
+                res.render("secrets");
+            } else {
+                res.send("Wong password");
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+app.route("/register")
+
+.get(function(req, res) {
+    res.render("register");
+})
+
+.post(function(req, res){
+    const user = new User({
+        email : req.body.username,
+        password : md5(req.body.password)
+    });
+
+    user.save(function(err){
+        if(!err) {
+            res.render("secrets");
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server is started at port 3000");
+});
